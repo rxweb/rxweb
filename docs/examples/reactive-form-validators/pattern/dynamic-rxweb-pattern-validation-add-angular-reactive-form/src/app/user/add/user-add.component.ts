27e73f4dc6fb8e483daf065eb76c5a1add2fdfca
ishani.shah <ishani.shah@radixweb.com>
2018-07-27 18:52:55 +0530

    commit for pattern

---
+++ docs/examples/reactive-form-validators/pattern/dynamic-rxweb-pattern-validation-add-angular-reactive-form/src/app/user/add/user-add.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from "@angular/forms"
+
+import { RxFormBuilder } from '@rxweb/reactive-form-validators';
+import { FormBuilderConfiguration  } from '@rxweb/reactive-form-validators';
+
+import { User } from '../user.model';
+
+@Component({
+    selector: 'app-user-add',
+    templateUrl: './user-add.component.html'
+})
+export class UserAddComponent implements OnInit {
+
+    userFormGroup: FormGroup
+
+    constructor(
+        private formBuilder: RxFormBuilder
+    ) { }
+
+    ngOnInit() {
+        let user = new User();
+        let formBuilderConfiguration = new FormBuilderConfiguration();
+        formBuilderConfiguration.validations = {
+			userName : {
+				pattern :  {pattern:{'onlyAlpha': /^[A-z]+$/},} 
+			},
+			zipCode : {
+				pattern :  {pattern:{'zipCode':/^\d{5}(?:[-\s]\d{4})?$/ },message:'Zipcode must be 5 digits',} 
+			},
+			age : {
+				pattern :  {pattern:{'onlyDigit': /^[0-9]*$/},conditionalExpressions:'x=>x.userName=="John"',} 
+			},
+        };
+		this.userFormGroup = this.formBuilder.formGroup(user,formBuilderConfiguration);
+    }
+}
