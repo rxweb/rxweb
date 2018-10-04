4c83ad0a0cdfd1403d97c4f318a2ba316e0156f8
ishani.shah <ishani.shah@radixweb.com>
2018-10-04 11:55:16 +0530

    commit changes

--- rxweb.io/src/app/components/dashboard/dashboard.component.ts
+++ rxweb.io/src/app/components/dashboard/dashboard.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnChanges, SimpleChanges, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
-  templateUrl: './dashboard.component.html',
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnChanges {
 
-
+  validators: any
+  @Input() searchText: any;
+  masterList: any;
   constructor(
-    private router: Router
+    private router: Router, private http: HttpClient
   ) {
   }
 
-  ngOnInit(): void {
+  ngOnChanges(change: any) {
+    if (change.searchText.currentValue)
+      this.filterList(change.searchText.currentValue);
+    else
+      this.validators = this.masterList;
   }
 
+  filterList(search: any) {
+    this.validators = this.masterList.filter(t => t.title.toLowerCase().startsWith(search.toLowerCase()))
+  }
+
+  ngOnInit(): void {
+    this.http.get('assets/validation.json')
+      .subscribe(response => {
+        this.validators = response;
+        this.masterList = this.validators.map(x => Object.assign({}, x));
+      });
+  }
 
   logOut(): void {
   }
