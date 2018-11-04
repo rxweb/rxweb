a6cf0fbaccafc074c5faacb1dd552bbe8f4ae140
Ajay Ojha <ojhaajay@outlook.com>
2018-11-05 00:00:13 +0530

    code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -177,8 +177,42 @@ testFormGroup:FormGroup;
 testForm:FormGroup;
 userForm:FormGroup;
 modelRuleGroup:FormGroup;
+validatorJson = [{
+  "fieldId":1,
+  "type":'text',
+  "label":"Username",
+  "placeHolder":"Enter your username",
+  "value":"",
+  "sortId":1,
+  "validation":{
+    "value":{
+      "alpha":true, 
+      "required":true,
+      "maxLength":{"value":14}
+    }
+  }
+},{
+  "fieldId":1,
+  "label":"password",
+  "placeHolder":"Password",
+  "value":"",
+  "sortId":1,
+  "validation":{
+    "value":{
+      "alpha":true, 
+      "required":true,
+      "maxLength":10
+    }
+  }
+}]
+dynamicFormGroup :FormGroup;
   ngOnInit() {
 let address = new Address();
+this.dynamicFormGroup  = this.validation.group({questions:this.validatorJson},{
+//excludeProps:['questions.fieldId','questions.label','questions.placeHolder','questions.sortId','questions.validation'],
+includeProps:['questions','questions.value'],
+dynamicValidationConfigurationPropertyName:'validation'});
+console.log(this.dynamicFormGroup);
 this.modelRuleGroup = this.validation.formGroup(address);
 this.userForm = this.formBuilder.group({
   nationality:[''],
@@ -191,6 +225,7 @@ RxwebValidators.minLength({value:11})
 conditionalExpression:(x) => x.nationality == 'Abroad' })
     ]]
 });
+
 this.testForm = this.formBuilder.group({
   password:['',[RxwebValidators.password ({
         validation:{
@@ -203,7 +238,8 @@ this.testForm = this.formBuilder.group({
   age:['',RxwebValidators.startsWith({value:"n"})],
   cardType:[''],
   creditCard:['',RxwebValidators.creditCard({fieldName:'cardType'})],
-  amount:['',[RxwebValidators.required(),RxwebValidators.numeric({allowDecimal:true,digitsInfo:'3.1-5',isFormat:true})]]
+  amount:['',[RxwebValidators.required(),RxwebValidators.numeric({allowDecimal:true,digitsInfo:'3.1-5',isFormat:true})]],
+fileData:['',RxwebValidators.extension({extensions:[".jpg"]})]
   
 });
         this.angularFormGroup = this.validation.group({
