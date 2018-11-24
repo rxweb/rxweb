f064320d1e51b6050b6030bfd0b4993bff099707
Ajay Ojha <ojhaajay@outlook.com>
2018-11-24 07:52:57 +0530

    config changes

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,9 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+  mime: {
+      'text/x-typescript': ['ts','tsx']
+    }
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly'],
