35b36538d43ccdec74a08da0f80fb54fcd96187b
ushmi.dave <ushmi.dave@PC0304>
2018-12-13 20:29:52 +0530

    commit alphanumeric spec

--- test/reactive-form-validators/rxweb-validators/alpha.validator.spec.ts
+++ test/reactive-form-validators/rxweb-validators/alpha.validator.spec.ts
@@ -1,69 +1,122 @@
 import {AbstractControl, AsyncValidatorFn, FormBuilder, FormArray, FormControl, Validators} from '@angular/forms';
 
-import { RxwebValidators,ReactiveFormConfig } from '../../../packages/reactive-form-validators';
+import { RxwebValidators,ReactiveFormConfig  } from '../../../packages/reactive-form-validators';
 
 (function() {
   describe('RxwebValidators', () => {
     beforeEach(() => {
       ReactiveFormConfig.set({
-            "validationMessage": {
-                "alpha": "only accepts alphabets.",
-            }
-        });
-   });
+        "validationMessage": {
+          "alpha": "Only alphabets are allowed.",
+        }
+      });
+    });
 
     describe('alpha', () => {
-      it('should not error on an empty string.',
-         () => { expect(RxwebValidators.alpha()(new FormControl(''))).toBeNull();
-     });
 
-      it('should not error on any value in alphabets.',
-         () => { expect(RxwebValidators.alpha()(new FormControl('India'))).toBeNull(); });
-
-      it('should not allow other than alphabets.',
-         () => { expect(RxwebValidators.alpha()(new FormControl('India@'))).toEqual({'alpha':{ message: 'only accepts alphabets.', refValues: [ 'India@' ] } }); });
-
+	      it('should not error on an empty string.',
+        () => { 
+          expect(RxwebValidators.alpha()(new FormControl(''))).toBeNull();
+        });
+	
       it('should not error on null.',
-         () => { expect(RxwebValidators.alpha()(new FormControl(null))).toBeNull(); });
-
+        () => { 
+          expect(RxwebValidators.alpha()(new FormControl(null))).toBeNull();
+        });
+	
       it('should not error on undefined.',
-         () => { expect(RxwebValidators.alpha()(new FormControl(undefined))).toBeNull(); });
+        () => { 
+          expect(RxwebValidators.alpha()(new FormControl(undefined))).toBeNull(); 
+        });
 
-      it('should allowWhitespace.',
-         () => { expect(RxwebValidators.alpha({allowWhiteSpace:true})(new FormControl("New Delhi"))).toBeNull(); });
-      
-      it('should not allow whitespace with "allowWhiteSpace" parameter value is false.',
-         () => { expect(RxwebValidators.alpha({allowWhiteSpace:false})(new FormControl("New Delhi"))).toEqual({'alpha':{ message: 'only accepts alphabets.', refValues: [ 'New Delhi' ] } })});
+      it("Should not error, If you want to apply conditional validation on 'Country Code' or 'City' then you need to add this input as 'India'.",
+        () => { 
+          expect(RxwebValidators.alpha()(new FormControl('India'))).toBeNull(); 
+        });
 
-      it('should not allowWhitespace.',
-         () => { expect(RxwebValidators.alpha()(new FormControl("New Delhi"))).toEqual({'alpha':{ message: 'only accepts alphabets.', refValues: [ 'New Delhi' ] } })});
 
-      it('show custom error message only.',
-         () => { expect(RxwebValidators.alpha({message:'Only accepts alphabets without space.'})(new FormControl("New Delhi"))).toEqual({'alpha':{ message: 'Only accepts alphabets without space.', refValues: [ 'New Delhi' ] } })});
+      it("Should error, If you want to apply conditional validation on 'Country Code' or 'City' then you need to add this input as 'India'.",
+        () => { 
+          expect(RxwebValidators.alpha()(new FormControl('India@'))).toEqual({'alpha':{ message: 'Only alphabets are allowed.', refValues: [ 'India@' ] } }); 
+        });
 
-      it('should not error on empty config object.',
-         () => { expect(RxwebValidators.alpha({})(new FormControl("India"))).toBeNull()});
-
-      it('should not error if country is India in countryName FormControl and alphabets in stateName FormControl.',
+      it("Should not error, Conditional Expression with type 'function'",
          () => {
           let formBuilder = new FormBuilder();
           let formGroup = formBuilder.group({
-            'countryName':['India'],
-            'stateName':'Gujarat'
+'countryName':['India'],
+            'countryCode':'IN'
           });
-          expect(RxwebValidators.alpha({conditionalExpression:(x,y)=> x.countryName == 'India' })(formGroup.controls.stateName)).toBeNull()
+          expect(RxwebValidators.alpha({conditionalExpression:(x,y) => x.countryName == "India"})(formGroup.controls.countryCode)).toBeNull()
         });
 
-      it('should give error if country is India in countryName FormControl and non alphabets characters are in stateName FormControl.',
+      it("Should not error, Conditional Expression with type 'function'",
          () => {
           let formBuilder = new FormBuilder();
           let formGroup = formBuilder.group({
-            'countryName':['India'],
-            'stateName':'Gujarat@'
+'countryName':['France'],
+            'countryCode':'IN01'
           });
-          expect(RxwebValidators.alpha({conditionalExpression:(x,y)=> x.countryName == 'India' })(formGroup.controls.stateName)).toEqual({'alpha':{ message: 'only accepts alphabets.', refValues: [ 'Gujarat@' ] } })
+          expect(RxwebValidators.alpha({conditionalExpression:(x,y) => x.countryName == "India"})(formGroup.controls.countryCode)).toBeNull()
         });
 
+
+      it("Should error, Conditional Expression with type 'function'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+			'countryName':['India'],
+            'countryCode':'IN@'
+          });
+          expect(RxwebValidators.alpha({conditionalExpression:(x,y) => x.countryName == "India"})(formGroup.controls.countryCode)).toEqual({'alpha':{ message: 'Only alphabets are allowed.', refValues: [ 'IN@' ] } }); 
+        });
+
+      it("Should not error, Conditional Expression with type 'string'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+'countryName':['India'],
+            'cityName':'GOA'
+          });
+          expect(RxwebValidators.alpha({conditionalExpression:'x => x.countryName =="India"'})(formGroup.controls.cityName)).toBeNull()
+        });
+
+      it("Should not error, Conditional Expression with type 'string'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+'countryName':['France'],
+            'cityName':'New Delhi'
+          });
+          expect(RxwebValidators.alpha({conditionalExpression:'x => x.countryName =="India"'})(formGroup.controls.cityName)).toBeNull()
+        });
+
+
+      it("Should error, Conditional Expression with type 'string'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+			'countryName':['India'],
+            'cityName':'New Delhi'
+          });
+          expect(RxwebValidators.alpha({conditionalExpression:'x => x.countryName =="India"'})(formGroup.controls.cityName)).toEqual({'alpha':{ message: 'Only alphabets are allowed.', refValues: [ 'New Delhi' ] } }); 
+        });
+
+      it("Should not error, This allowed whitespace in alpha validation",
+        () => { 
+          expect(RxwebValidators.alpha({allowWhiteSpace:true})(new FormControl('New Delhi'))).toBeNull(); 
+        });
+
+
+      it("Should error, Shows custom message",
+        () => { 
+          expect(RxwebValidators.alpha({message:'You can enter only alphabets.'})(new FormControl('DL01'))).toEqual({'alpha':{ message: 'You can enter only alphabets.', refValues: [ 'DL01' ] } }); 
+        });
+
+
+
+	//end
     });
   });
 })();
+
---
+++ test/reactive-form-validators/rxweb-validators/alphaNumeric.validator.spec.ts
@@ -0,0 +1,111 @@
+import {AbstractControl, AsyncValidatorFn, FormBuilder, FormArray, FormControl, Validators} from '@angular/forms';
+
+import { RxwebValidators,ReactiveFormConfig  } from '../../../packages/reactive-form-validators';
+
+(function() {
+  describe('RxwebValidators', () => {
+    beforeEach(() => {
+      ReactiveFormConfig.set({
+        "validationMessage": {
+          "alphaNumeric": "Only Alpha Numerics are allowed.",
+        }
+      });
+    });
+
+    describe('alphaNumeric', () => {
+
+	      it('should not error on an empty string.',
+        () => { 
+          expect(RxwebValidators.alphaNumeric()(new FormControl(''))).toBeNull();
+        });
+	
+      it('should not error on null.',
+        () => { 
+          expect(RxwebValidators.alphaNumeric()(new FormControl(null))).toBeNull();
+        });
+	
+      it('should not error on undefined.',
+        () => { 
+          expect(RxwebValidators.alphaNumeric()(new FormControl(undefined))).toBeNull(); 
+        });
+
+      it("Should not error, This allowed whitespace in alpha Numeric validation",
+        () => { 
+          expect(RxwebValidators.alphaNumeric({allowWhiteSpace:true})(new FormControl('Victoria Park'))).toBeNull(); 
+        });
+
+
+      it("Should error, Shows custom message",
+        () => { 
+          expect(RxwebValidators.alphaNumeric({message:'Please enter only alphanumerics, special characters are not allowed.'})(new FormControl('1600 Amphi-theatre Pkwy'))).toEqual({'alphaNumeric':{ message: 'Please enter only alphanumerics, special characters are not allowed.', refValues: [ '1600 Amphi-theatre Pkwy' ] } }); 
+        });
+
+      it("Should not error, Conditional Expression with type 'function'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+'areaName':['Delhi'],
+            'countryCode':'IN'
+          });
+          expect(RxwebValidators.alphaNumeric({conditionalExpression:(x,y) => x.areaName == "Delhi" })(formGroup.controls.countryCode)).toBeNull()
+        });
+
+      it("Should not error, Conditional Expression with type 'function'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+'areaName':['Mumbai'],
+            'countryCode':'IN@1'
+          });
+          expect(RxwebValidators.alphaNumeric({conditionalExpression:(x,y) => x.areaName == "Delhi" })(formGroup.controls.countryCode)).toBeNull()
+        });
+
+
+      it("Should error, Conditional Expression with type 'function'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+			'areaName':['Delhi'],
+            'countryCode':'IN@'
+          });
+          expect(RxwebValidators.alphaNumeric({conditionalExpression:(x,y) => x.areaName == "Delhi" })(formGroup.controls.countryCode)).toEqual({'alphaNumeric':{ message: 'Only Alpha Numerics are allowed.', refValues: [ 'IN@' ] } }); 
+        });
+
+      it("Should not error, Conditional Expression with type 'string'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+'areaName':['Delhi'],
+            'cityCode':'NY'
+          });
+          expect(RxwebValidators.alphaNumeric({conditionalExpression:'x => x.areaName =="Delhi"'})(formGroup.controls.cityCode)).toBeNull()
+        });
+
+      it("Should not error, Conditional Expression with type 'string'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+'areaName':['Mumbai'],
+            'cityCode':'NY#1'
+          });
+          expect(RxwebValidators.alphaNumeric({conditionalExpression:'x => x.areaName =="Delhi"'})(formGroup.controls.cityCode)).toBeNull()
+        });
+
+
+      it("Should error, Conditional Expression with type 'string'",
+         () => {
+          let formBuilder = new FormBuilder();
+          let formGroup = formBuilder.group({
+			'areaName':['Delhi'],
+            'cityCode':'NY@'
+          });
+          expect(RxwebValidators.alphaNumeric({conditionalExpression:'x => x.areaName =="Delhi"'})(formGroup.controls.cityCode)).toEqual({'alphaNumeric':{ message: 'Only Alpha Numerics are allowed.', refValues: [ 'NY@' ] } }); 
+        });
+
+
+
+	//end
+    });
+  });
+})();
+
