a48de370b1afcef4023e78c8b7f88861569cebd0
ushmi.dave <ushmi.dave@PC0304>
2018-12-15 11:15:12 +0530

    commit functional-validator.spec

---
+++ test/functional-validatiors/functional.validator.spec.ts
@@ -0,0 +1,12 @@
+import {AbstractControl, AsyncValidatorFn, FormBuilder, FormArray, FormControl, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+
+(function()
+{
+    describe('AppComponent',() =>
+    {
+        let component : Component;
+        
+    })
+})
+
