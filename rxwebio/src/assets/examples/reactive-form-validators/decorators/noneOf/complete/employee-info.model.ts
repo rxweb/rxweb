ef46ea1cff1e0100de2ec8a6d70adcff430f5480
srishti.khandelwal <srishti.khandelwal@PC0307>
2019-01-10 11:15:31 +0530

    perf(json): updating decorators and validators json of allOf, oneOf, noneOf and choice

--- rxwebio/src/assets/examples/reactive-form-validators/decorators/noneOf/complete/employee-info.model.ts
+++ rxwebio/src/assets/examples/reactive-form-validators/decorators/noneOf/complete/employee-info.model.ts
@@ -14,6 +14,6 @@ export class EmployeeInfo{
     @noneOf({matchValues: ["MVC", "AngularJS","Angular 5","C#","Web Api","SQL Server"], conditionalExpression: "x => x.department =='DotNet'"})
     skills: string;
 
-    @noneOf({matchValues:["Drawing", "Singing","Dancing","Travelling","Sports"],message: "Please select no hobbies"})
+    @noneOf({matchValues:["Drawing", "Singing","Dancing","Travelling","Sports"],message: "Please do not select any hobbies"})
     hobbies: string;
 }
--- rxwebio/src/assets/examples/reactive-form-validators/template-driven/validation-directives/choice/minLength/employee-info.model.ts
+++ rxwebio/src/assets/examples/reactive-form-validators/template-driven/validation-directives/choice/minLength/employee-info.model.ts
@@ -2,6 +2,6 @@ import {  choice  } from "@rxweb/reactive-form-validators"
 
 export class EmployeeInfo{
 
-    @choice({maxLength:4})
+    @choice({minLength:3})
     qualifications: string[] = [];
 }
