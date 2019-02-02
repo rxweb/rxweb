1cf289960d081ceb1a02743fa47f956dff85d936
ajayojha <ojhaajay@outlook.com>
2019-02-02 21:03:59 +0530

    feat(template-driven): write file object in FormControl as a value
    feat #104

--- packages/reactive-form-validators/directives/template-validations/file-control.directive.ts
+++ packages/reactive-form-validators/directives/template-validations/file-control.directive.ts
@@ -1,60 +1,86 @@
-import {Input, Directive ,forwardRef,ElementRef} from '@angular/core';
-import { AbstractControl, NG_VALIDATORS,Validator,ValidatorFn } from '@angular/forms';
-import {ExtensionConfig,SizeConfig,FileConfig} from '../../models/config'
+import { Input, Directive, forwardRef, ElementRef } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { ExtensionConfig, SizeConfig, FileConfig } from '../../models/config'
 import { APP_VALIDATORS } from "../../const/app-validators.const";
 const VALIDATOR_CONFIG = "validatorConfig";
-const FILE_VALIDATOR_NAMES:string[] = ["extension","fileSize","file"];
+const FILE_VALIDATOR_NAMES: string[] = ["extension", "fileSize", "file"];
 @Directive({
     selector: "input[type=file]",
-    providers: [{
+    host: {
+        "(change)": "onChangeCall($event.target)",
+        "(blur)": "onTouched()"
+    },
+    providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: FileControlDirective, multi: true }, {
         provide: NG_VALIDATORS,
         useExisting: forwardRef(() => FileControlDirective),
         multi: true
     }]
 })
 export class FileControlDirective implements Validator {
-    files:any;
-    element:any
-    isProcessed:boolean = false;
-    private validators:Function[] = [];
+    files: any;
+    element: any
+    isProcessed: boolean = false;
+    
 
-    @Input() set extension(config:ExtensionConfig){
-      this.pushValidator(FILE_VALIDATOR_NAMES[0],config)
-    }
-    @Input() set fileSize(config:SizeConfig){
-      this.pushValidator(FILE_VALIDATOR_NAMES[1],config)
+    private validators: Function[] = [];
+
+    onChange = (_) => { };
+    onTouched = () => { };
+
+    onChangeCall(element: HTMLInputElement) {
+        let files = element.files;
+        if (this.writeFile)
+            this.onChange(files);
+        else {
+            if (files.length > 0)
+                this.onChange(element.value);
+            else
+                this.onChange(undefined);
+        }
     }
 
-    @Input() set file(config:FileConfig){
-      this.pushValidator(FILE_VALIDATOR_NAMES[2],config)
+    writeValue(value) { }
+    registerOnChange(invocation: any) { this.onChange = invocation; }
+    registerOnTouched(invocation: any) { this.onTouched = invocation; }
+
+    @Input() writeFile: boolean;
+    @Input() set extension(config: ExtensionConfig) {
+        this.pushValidator(FILE_VALIDATOR_NAMES[0], config)
+    }
+    @Input() set fileSize(config: SizeConfig) {
+        this.pushValidator(FILE_VALIDATOR_NAMES[1], config)
     }
 
-    constructor(private elementRef: ElementRef){
+    @Input() set file(config: FileConfig) {
+        this.pushValidator(FILE_VALIDATOR_NAMES[2], config)
+    }
+
+    constructor(private elementRef: ElementRef) {
         this.element = elementRef.nativeElement as Node;
     }
 
-    setConfig(control:AbstractControl){
-      FILE_VALIDATOR_NAMES.forEach(t=>{
-        if(!this[t] && control[VALIDATOR_CONFIG] && control[VALIDATOR_CONFIG][t])
-          this[t] = control[VALIDATOR_CONFIG][t];
-      })
-      this.isProcessed = true;
+    setConfig(control: AbstractControl) {
+        FILE_VALIDATOR_NAMES.forEach(t => {
+            if (!this[t] && control[VALIDATOR_CONFIG] && control[VALIDATOR_CONFIG][t])
+                this[t] = control[VALIDATOR_CONFIG][t];
+        })
+        this.isProcessed = true;
     }
 
-    pushValidator(validatorName:string,config:ExtensionConfig | SizeConfig | FileConfig){
-      if(config)
-        this.validators.push(APP_VALIDATORS[validatorName](config))
+    pushValidator(validatorName: string, config: ExtensionConfig | SizeConfig | FileConfig) {
+        if (config)
+            this.validators.push(APP_VALIDATORS[validatorName](config))
     }
-    
+
     validate(control: AbstractControl): { [key: string]: any } {
-      if(!this.isProcessed)
-        this.setConfig(control);
-      var result = null;
-      for(var validator of this.validators){
-          result = validator(control,this.element.files);
-          if(result)
-            break;
-      }
-      return result 
-    }    
+        if (!this.isProcessed)
+            this.setConfig(control);
+        var result = null;
+        for (var validator of this.validators) {
+            result = validator(control, this.element.files);
+            if (result)
+                break;
+        }
+        return result
+    }
 }
