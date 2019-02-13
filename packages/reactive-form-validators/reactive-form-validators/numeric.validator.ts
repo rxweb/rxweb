190f2dc8d69fe5535fbb3c8be065f023faff61a6
ajayojha <ojhaajay@outlook.com>
2019-02-13 15:20:23 +0530

    fix(decorator): currency format fixes for model based validation
    fix #122

--- packages/reactive-form-validators/reactive-form-validators/numeric.validator.ts
+++ packages/reactive-form-validators/reactive-form-validators/numeric.validator.ts
@@ -1,6 +1,6 @@
 import {
-  ValidatorFn,
-  AbstractControl
+    ValidatorFn,
+    AbstractControl
 } from "@angular/forms";
 
 import { RegexValidator } from "../util/regex-validator";
@@ -9,13 +9,16 @@ import { NumericConfig } from "../models/config/numeric-config";
 import { AnnotationTypes } from "../core/validator.static";
 import { ApplicationUtil } from '../util/app-util';
 import { ValidatorValueChecker } from "../util/validator-value-checker";
+import { VALIDATOR_CONFIG  } from "../const/app.const"
 export function numericValidator(config: NumericConfig): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } => {
-    config = ApplicationUtil.getConfigObject(config);
-    if (ValidatorValueChecker.pass(control, config)) {
-      if (!RegexValidator.isValid(control.value, ApplicationUtil.numericValidation(config.allowDecimal, config.acceptValue)))
-        return ObjectMaker.toJson(AnnotationTypes.numeric, config, [control.value]);
+    return (control: AbstractControl): { [key: string]: any } => {
+        if (config && (!control[VALIDATOR_CONFIG] || !control[VALIDATOR_CONFIG][AnnotationTypes.numeric]))
+            ApplicationUtil.configureControl(control, config, AnnotationTypes.numeric);
+        config = ApplicationUtil.getConfigObject(config);
+        if (ValidatorValueChecker.pass(control, config)) {
+            if (!RegexValidator.isValid(control.value, ApplicationUtil.numericValidation(config.allowDecimal, config.acceptValue)))
+                return ObjectMaker.toJson(AnnotationTypes.numeric, config, [control.value]);
+        }
+        return ObjectMaker.null();
     }
-    return ObjectMaker.null();
-  }
 }
