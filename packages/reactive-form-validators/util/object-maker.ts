e229bdc6fcf02c210090045927f920f82fb6ce0b
Ajay Ojha <ojhaajay@outlook.com>
2019-02-21 08:22:53 +0530

    feat(validator): add new property of 'messageKey'
    Showing custom message globally based on key.
    
    feat #132

--- packages/reactive-form-validators/util/object-maker.ts
+++ packages/reactive-form-validators/util/object-maker.ts
@@ -3,7 +3,10 @@ import { ReactiveFormConfig } from "./reactive-form-config";
 export class ObjectMaker{
     static toJson(key: string, config:any, values: any) {
         let message = config ? config.message : null;
-        let messageText = (message) ? message : (ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.validationMessage && ReactiveFormConfig.json.validationMessage[key])? ReactiveFormConfig.json.validationMessage[key] : '';
+        let messageKey = undefined;
+        if(!message && config && config.messageKey)
+            messageKey = config.messageKey;
+        let messageText = (message) ? message : (ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.validationMessage && ReactiveFormConfig.json.validationMessage[messageKey || key])? ReactiveFormConfig.json.validationMessage[messageKey || key] : '';
         values.forEach((t ,index)=> {
             messageText = messageText.replace(`{{${index}}}`, t);
         });
