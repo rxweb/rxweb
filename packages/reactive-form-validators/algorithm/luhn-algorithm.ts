6f73aaaf925038bd0f896510400a6cd2a86e5f36
ajayojha <ojhaajay@outlook.com>
2019-03-07 21:09:24 +0530

    fix(validator): validate 19 digit discover card numbers

---
+++ packages/reactive-form-validators/algorithm/luhn-algorithm.ts
@@ -0,0 +1,15 @@
+﻿export function calculate(numbers: string) {
+    let numberSum = 0;
+    for (var i = 0; i < numbers.length; i++) numberSum += parseInt(numbers.substring(i, i + 1));
+
+    let deltas = new Array(0, 1, 2, 3, 4, -4, -3, -2, -1, 0);
+    for (var i = numbers.length - 1; i >= 0; i -= 2) {
+        numberSum += deltas[parseInt(numbers.substring(i, i + 1))];
+    }
+
+    let mod = numberSum % 10;
+    mod = 10 - mod;
+    if (mod == 10)
+        mod = 0;
+    return mod;
+}
--- packages/reactive-form-validators/reactive-form-validators/credit-card.validator.ts
+++ packages/reactive-form-validators/reactive-form-validators/credit-card.validator.ts
@@ -1,6 +1,6 @@
 import {
-  ValidatorFn,
-  AbstractControl
+    ValidatorFn,
+    AbstractControl
 } from "@angular/forms";
 
 import { RegexValidator } from "../util/regex-validator";
@@ -11,48 +11,48 @@ import { ApplicationUtil } from "../util/app-util";
 import { AnnotationTypes } from "../core/validator.static";
 import { FormProvider } from '../util/form-provider';
 import { checkLength } from '../util/check-length'
+import { calculate } from '../algorithm/luhn-algorithm'
 
 export function creditCardValidator(config: CreditCardConfig): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } => {
-    const controlValue = control.value;
-    config = ApplicationUtil.getConfigObject(config);
-    const parentObject = (control.parent) ? control.parent.value : undefined;
-    if (FormProvider.ProcessRule(control, config)) {
-      if (RegexValidator.isNotBlank(controlValue)) {
-        let isValid = false;
-        let cardTypes = config.fieldName && parentObject[config.fieldName] ? [parentObject[config.fieldName]] : config.creditCardTypes
-        let cardType: string = '';
-        for (let creditCardType of cardTypes) {
-          switch (creditCardType) {
-            case "AmericanExpress":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.AmericanExpress) && checkLength(controlValue.length,[15]);
-              break;
-            case "DinersClub":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.DinersClub) && checkLength(controlValue.length, [14,16,19]);
-              break;
-            case "Discover":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.Discover) && checkLength(controlValue.length, [16,19]);
-              break;
-            case "JCB":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.JCB) && checkLength(controlValue.length, [16,19]);
-              break;
-            case "Maestro":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.Maestro) && checkLength(controlValue.length, [12,16,19]);
-              break;
-            case "MasterCard":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.MasterCard) && checkLength(controlValue.length, [16]);
-              break;
-            case "Visa":
-              isValid = RegexValidator.isValid(controlValue, RegExRule.creditCard.Visa) && checkLength(controlValue.length, [13,16,19]);
-              break;
-          }
-          cardType = creditCardType;
-        }
-        if (!isValid)
-          return ObjectMaker.toJson(AnnotationTypes.creditCard, config, [controlValue, cardType])
-      }
+    let cardDigits: { [key: string]: number[] } = {
+        AmericanExpress: [15],
+        DinersClub: [14, 16, 19],
+        Discover: [16, 19],
+        JCB: [16, 19],
+        Maestro: [12, 16, 19],
+        MasterCard: [16],
+        Visa: [13, 16, 19]
     }
-    return ObjectMaker.null();
+    function validate(creditCardNumber: string) {
+        var digit = parseInt(creditCardNumber.substring(creditCardNumber.length - 1, creditCardNumber.length));
+        return calculate(creditCardNumber.substring(0, creditCardNumber.length - 1)) == parseInt(String(digit)) ? !0 : !1
+    }
 
-  }
+    function getCardProviderName(cardNumber:string) {
+        var cardProviderName = "";
+        return /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)\d+$/.test(cardNumber) ? cardProviderName = "Maestro" : /^5[1-5]/.test(cardNumber) ? cardProviderName = "MasterCard" : /^4/.test(cardNumber) ? cardProviderName = "Visa" : /^3[47]/.test(cardNumber) ? cardProviderName = "AmericanExpress" : /^(?:2131|1800|35)/.test(cardNumber) ? cardProviderName = "JCB" : /^3(?:0[0-5]|[68])/.test(cardNumber) ? cardProviderName = "DinersClub" : /^6(?:011|5)/.test(cardNumber) && (cardProviderName = "Discover"), cardProviderName;
+    }
+
+    return (control: AbstractControl): { [key: string]: any } => {
+        const controlValue = control.value;
+        config = ApplicationUtil.getConfigObject(config);
+        const parentObject = (control.parent) ? control.parent.value : undefined;
+        if (FormProvider.ProcessRule(control, config)) {
+            if (RegexValidator.isNotBlank(controlValue)) {
+                let isValid = false;
+                let cardTypes = config.fieldName && parentObject[config.fieldName] ? [parentObject[config.fieldName]] : config.creditCardTypes
+                let cardType: string = '';
+                for (let creditCardType of cardTypes) {
+                    isValid = checkLength(controlValue.length, cardDigits[creditCardType]) && getCardProviderName(controlValue) == creditCardType && validate(controlValue);
+                    cardType = creditCardType;
+                    if (isValid)
+                        break;
+                }
+                if (!isValid)
+                    return ObjectMaker.toJson(AnnotationTypes.creditCard, config, [controlValue, cardType])
+            }
+        }
+        return ObjectMaker.null();
+
+    }
 }
