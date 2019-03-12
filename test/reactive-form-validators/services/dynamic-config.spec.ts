2544f602bd5bee23130ad50ad80d75cb6ada7547
ajayojha <ojhaajay@outlook.com>
2019-03-12 13:18:06 +0530

    spec(validator): dynamic config validator based. spec: #142

--- test/reactive-form-validators/services/dynamic-config.spec.ts
+++ test/reactive-form-validators/services/dynamic-config.spec.ts
@@ -46,7 +46,44 @@ class User {
         userFormGroup.controls.age.setValue(17);
           userFormGroup.controls.amount.setValue(2600);
           expect(userFormGroup.controls.amount.errors).toEqual({'minNumber':{ message: 'Number should less than equal to minimum number.', refValues: [ 2600,3500 ] } }); 
+     })
+
+     it('should error, when the age control value is below 17 and amount is 2600 in validator based approach', () => {
+         let appFormBuilder = new FormBuilder();
+         let userFormGroup = appFormBuilder.group({
+             age: '',
+             amount: ['', RxwebValidators.minNumber({
+
+                 dynamicConfig: (x, y) => {
+                     if (x.age > 18)
+                         return { value: 2500 };
+                     else
+                         return { value: 3500 };
+                 }
+             })]
          })
+         userFormGroup.controls.age.setValue(17);
+         userFormGroup.controls.amount.setValue(2600);
+         expect(userFormGroup.controls.amount.errors).toEqual({ 'minNumber': { message: 'Number should less than equal to minimum number.', refValues: [2600, 3500] } });
+     })
+
+     it('should pass, when the age control value is above 18 and amount is 2600 in validator based approach', () => {
+         let appFormBuilder = new FormBuilder();
+         let userFormGroup = appFormBuilder.group({
+             age: '',
+             amount: ['', RxwebValidators.minNumber({
+                 dynamicConfig: (x, y) => {
+                     if (x.age > 18)
+                         return { value: 2500 };
+                     else
+                         return { value: 3500 };
+                 }
+             })]
+         })
+         userFormGroup.controls.age.setValue(19);
+         userFormGroup.controls.amount.setValue(2600);
+         expect(userFormGroup.controls.amount.errors).toBeNull();
+     })
 
 
 
