978b3790f920af7033c56adc2a335c59d55fabda
ajayojha <ojhaajay@outlook.com>
2019-03-28 20:00:52 +0530

    chore(validator): remove unused imports from email validator

--- packages/reactive-form-validators/reactive-form-validators/email.validator.ts
+++ packages/reactive-form-validators/reactive-form-validators/email.validator.ts
@@ -3,12 +3,8 @@ import {
   AbstractControl
 } from "@angular/forms";
 import { RegExRule } from "../util/regex-rules";
-import { RegexValidator } from "../util/regex-validator";
 import { EmailConfig } from "../models/config/email-config";
-import { ObjectMaker } from "../util/index";
 import { AnnotationTypes } from "../core/validator.static";
-import { ValidatorValueChecker } from "../util/validator-value-checker";
-import { getConfigObject } from "../util/config-provider"
 import { regexValidation } from "../validators-function/regex-validation.function"
 export function emailValidator(configModel: EmailConfig): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } => {
