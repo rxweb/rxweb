7198384ef6e95d5d60e768dfb0a0cd62c88c4687
Ajay Ojha <ojhaajay@outlook.com>
2019-05-05 16:22:06 +0530

    spec(service):nested formarray modification spec ref:#168

--- test/reactive-form-validators/services/modified-check.spec.ts
+++ test/reactive-form-validators/services/modified-check.spec.ts
@@ -1,6 +1,6 @@
 ﻿import {FormGroup, FormArray} from '@angular/forms';
 
-import { RxFormBuilder, RxFormGroup, RxFormControl,prop, propObject, propArray } from '@rxweb/reactive-form-validators';
+import { RxFormBuilder, RxFormGroup, RxFormControl,RxFormArray,prop, propObject, propArray } from '@rxweb/reactive-form-validators';
 
 export class Hobby {
 
@@ -67,6 +67,54 @@ export class User {
           expect((<RxFormGroup>userFormGroup).isModified).toBe(true);
       })
 
+      it('should pass, Nested formgroup `isModified` flag should be false', () => {
+          let user = new User();
+          user.address = new Address();
+          let userFormGroup = <RxFormGroup>formBuilder.formGroup(user);
+          let addressFormGroup = <RxFormGroup>userFormGroup.controls.address
+          expect((<RxFormControl>addressFormGroup.controls.name).isModified).toBe(false);
+          expect((<RxFormGroup>addressFormGroup).isModified).toBe(false);
+          expect((<RxFormGroup>userFormGroup).isModified).toBe(false);
+      })
+
+      it('should pass, Nested FormArray `isModified` flag should be false', () => {
+          let user = new User();
+          user.hobbies = new Array<Hobby>();
+          user.hobbies.push(new Hobby());
+          let userFormGroup = <RxFormGroup>formBuilder.formGroup(user);
+          let hobbiesFormArray = <RxFormArray>userFormGroup.controls.hobbies
+          let hobbyFormGroup = <RxFormGroup>hobbiesFormArray.controls[0];
+          expect((<RxFormControl>hobbyFormGroup.controls.name).isModified).toBe(false);
+          expect((<RxFormGroup>hobbyFormGroup).isModified).toBe(false);
+          expect((<RxFormGroup>userFormGroup).isModified).toBe(false);
+      })
+
+      it('should pass, Nested FormArray `isModified` flag should be true', () => {
+          let user = new User();
+          user.hobbies = new Array<Hobby>();
+          user.hobbies.push(new Hobby());
+          let userFormGroup = <RxFormGroup>formBuilder.formGroup(user);
+          let hobbiesFormArray = <RxFormArray>userFormGroup.controls.hobbies
+          let hobbyFormGroup = <RxFormGroup>hobbiesFormArray.controls[0];
+          hobbyFormGroup.controls.name.setValue("Chess");
+          expect((<RxFormControl>hobbyFormGroup.controls.name).isModified).toBe(true);
+          expect((<RxFormGroup>hobbyFormGroup).isModified).toBe(true);
+          expect((<RxFormGroup>userFormGroup).isModified).toBe(true);
+      })
+
+      it('should pass, Nested FormArray `isModified` flag should be true after removing last index', () => {
+          let user = new User();
+          user.hobbies = new Array<Hobby>();
+          user.hobbies.push(new Hobby());
+          user.hobbies.push(new Hobby());
+          let userFormGroup = <RxFormGroup>formBuilder.formGroup(user);
+          let hobbiesFormArray = <RxFormArray>userFormGroup.controls.hobbies
+          expect((<RxFormArray>hobbiesFormArray).isModified).toBe(false);
+          expect((<RxFormGroup>userFormGroup).isModified).toBe(false);
+          hobbiesFormArray.removeAt(1);
+          expect((<RxFormArray>hobbiesFormArray).isModified).toBe(true);
+          expect((<RxFormGroup>userFormGroup).isModified).toBe(true);
+      })
      
 
 })
