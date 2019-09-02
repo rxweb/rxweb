7d480f0ccb82ad9017f98b98c31930082d301f03
Ajay Ojha <ojhaajay@outlook.com>
2019-09-02 13:17:46 +0530

    fix(validator): fix length of undefined fix:#194

--- packages/reactive-form-validators/reactive-form-validators/file.validator.ts
+++ packages/reactive-form-validators/reactive-form-validators/file.validator.ts
@@ -12,16 +12,19 @@ import {getConfigObject} from "../util/config-provider";
 import { VALIDATOR_CONFIG } from "../const/app.const";
 export function fileValidator(configModel: FileConfig): any {
   return (control: AbstractControl, files: FileList): { [key: string]: any } => {
-    let config = getConfigObject(configModel,control);
+    let config = getConfigObject(configModel,control);    
     if (!control[VALIDATOR_CONFIG] || !control[VALIDATOR_CONFIG][AnnotationTypes.file])
-      ApplicationUtil.configureControl(control, config, AnnotationTypes.file);
-    if (FormProvider.ProcessRule(control, config)) {
-      if (RegexValidator.isNotBlank(control.value)) {
-        let minFiles = config.minFiles ? config.minFiles : 0;
-        let maxFiles = config.maxFiles ? config.maxFiles : files.length;
-        if (!(files.length > 0 && files[0] instanceof File && files.length >= minFiles && files.length <= maxFiles))
-          return ObjectMaker.toJson(AnnotationTypes.file, config, [files.length, minFiles, maxFiles]);
+        ApplicationUtil.configureControl(control, config, AnnotationTypes.file);
+    if(files){
+      if (FormProvider.ProcessRule(control, config)) {
+        if (RegexValidator.isNotBlank(control.value)) {
+          let minFiles = config.minFiles ? config.minFiles : 0;
+          let maxFiles = config.maxFiles ? config.maxFiles : files.length;
+          if (!(files.length > 0 && files[0] instanceof File && files.length >= minFiles && files.length <= maxFiles))
+            return ObjectMaker.toJson(AnnotationTypes.file, config, [files.length, minFiles, maxFiles]);
+        }
       }
-    } return ObjectMaker.null();
+    }
+     return ObjectMaker.null();
   }
 }
