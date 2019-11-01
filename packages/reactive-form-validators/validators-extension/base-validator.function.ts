9d5a8204e300f6467f82a70bd42f05b19d6940ed
ajayojha <ojhaajay@outlook.com>
2019-11-01 13:53:56 +0530

    fix(validator): binding validator name specifically to resolution for dynamic expression #231

--- packages/reactive-form-validators/validators-extension/base-validator.function.ts
+++ packages/reactive-form-validators/validators-extension/base-validator.function.ts
@@ -1,20 +1,22 @@
 import {
-  ValidatorFn
+    ValidatorFn
 } from "@angular/forms";
 import { defaultContainer } from "../core/defaultContainer"
 import { STRING } from '../const/validator.const';
 import { ApplicationUtil } from '../util/app-util';
+import { RX_WEB_VALIDATOR } from "../const";
 
 export function baseValidator(config: any, type: any, validator: Function): ValidatorFn {
-  var rxwebValidator = (control: any, target?: object): { [key: string]: any } => {
-    if (typeof control == STRING)
-      defaultContainer.init(target, 0, control, type, config,false);
-    else{
-      if (config && (!control.validatorConfig || !control.validatorConfig[type])) 
-        ApplicationUtil.configureControl(control, config, type);
-      return  validator(control);
+    var rxwebValidator = (control: any, target?: object): { [key: string]: any } => {
+        if (typeof control == STRING)
+            defaultContainer.init(target, 0, control, type, config,false);
+        else{
+            if (config && (!control.validatorConfig || !control.validatorConfig[type]))
+                ApplicationUtil.configureControl(control, config, type);
+            return  validator(control);
+        }
+        return null
     }
-    return null
-  }
-  return rxwebValidator;
+    Object.defineProperty(rxwebValidator, "name", { value: RX_WEB_VALIDATOR });
+    return rxwebValidator;
 }
