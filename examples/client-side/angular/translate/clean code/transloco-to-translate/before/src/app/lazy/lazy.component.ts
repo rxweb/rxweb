d191f6828da010578105e22dd9a976ab3b51f4d8
ajayojha <ojhaajay@outlook.com>
2020-06-11 11:34:04 +0530

    before example added

---
+++ examples/client-side/angular/translate/clean code/transloco-to-translate/before/src/app/lazy/lazy.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy',
+  templateUrl: './lazy.component.html'
+})
+export class LazyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ examples/client-side/angular/translate/clean code/transloco-to-translate/before/src/app/scope-sharing/scope-sharing.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scope-sharing',
+  templateUrl: './scope-sharing.component.html',
+  styleUrls: []
+})
+export class ScopeSharingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
