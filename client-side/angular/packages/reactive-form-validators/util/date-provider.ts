52fb3f2b3d1871b7ab8b9a78a29211f026c504f9
ajayojha <ojhaajay@outlook.com>
2020-08-18 14:00:13 +0530

    fix(validator): FormControl must be valid only if configured separator is match #333

--- client-side/angular/packages/reactive-form-validators/util/date-provider.ts
+++ client-side/angular/packages/reactive-form-validators/util/date-provider.ts
@@ -1,100 +1,101 @@
 import { ReactiveFormConfig } from "./reactive-form-config";
-import {ApplicationUtil } from './app-util'
+import { ApplicationUtil } from './app-util'
 const ISO_DATE_REGEX = /^(?:[\+-]?\d{4}(?!\d{2}\b))(?:(-?)(?:(?:0[1-9]|1[0-2])(?:\1(?:[12]\d|0[1-9]|3[01]))?|W(?:[0-4]\d|5[0-2])(?:-?[1-7])?|(?:00[1-9]|0[1-9]\d|[12]\d{2}|3(?:[0-5]\d|6[1-6])))(?:[T\s](?:(?:(?:[01]\d|2[0-3])(?:(:?)[0-5]\d)?|24\:?00)(?:[\.,]\d+(?!:))?)?(?:\2[0-5]\d(?:[\.,]\d+)?)?(?:[zZ]|(?:[\+-])(?:[01]\d|2[0-3]):?(?:[0-5]\d)?)?)?)?$/;
-export class DateProvider{
+export class DateProvider {
 
-  isDate(value: any): Boolean {
-    return value instanceof Date && !isNaN(value.valueOf());
-  }
+    isDate(value: any): Boolean {
+        return value instanceof Date && !isNaN(value.valueOf());
+    }
 
-    private getRegex(dateFormat:string) : RegExp{
-      var regExp:string;
-      switch(dateFormat){
+    private getRegex(dateFormat: string): RegExp {
+        var regExp: string;
+        switch (dateFormat) {
             case 'ymd':
-            regExp = "^(?:[0-9]{4})-(1[0-2]|0?[1-9])-(3[01]|[12][0-9]|0?[1-9])$";
-            break;
+                regExp = "^(?:[0-9]{4})-(1[0-2]|0?[1-9])-(3[01]|[12][0-9]|0?[1-9])$";
+                break;
             case 'dmy':
-            regExp = "^(3[01]|[12][0-9]|0?[1-9])-(1[0-2]|0?[1-9])-(?:[0-9]{2})?[0-9]{2}$";
-            break;
+                regExp = "^(3[01]|[12][0-9]|0?[1-9])-(1[0-2]|0?[1-9])-(?:[0-9]{2})?[0-9]{2}$";
+                break;
             case 'mdy':
-            regExp = "^(1[0-2]|0?[1-9])-(3[01]|[12][0-9]|0?[1-9])-(?:[0-9]{2})?[0-9]{2}$";
-            break;
-      }
-      return new RegExp(regExp);
+                regExp = "^(1[0-2]|0?[1-9])-(3[01]|[12][0-9]|0?[1-9])-(?:[0-9]{2})?[0-9]{2}$";
+                break;
+        }
+        return new RegExp(regExp);
     }
 
-    regex(){
-      var regExp:RegExp;
-      if(ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.internationalization && ReactiveFormConfig.json.internationalization.dateFormat  && ReactiveFormConfig.json.internationalization.seperator)
-        regExp = this.getRegex(ReactiveFormConfig.json.internationalization.dateFormat)
-      else
-        regExp = (ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.baseConfig && ReactiveFormConfig.json.baseConfig.dateFormat) ? this.getRegex(ReactiveFormConfig.json.baseConfig.dateFormat) : this.getRegex("mdy");
-      return regExp;
+    regex() {
+        var regExp: RegExp;
+        if (ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.internationalization && ReactiveFormConfig.json.internationalization.dateFormat && ReactiveFormConfig.json.internationalization.seperator)
+            regExp = this.getRegex(ReactiveFormConfig.json.internationalization.dateFormat)
+        else
+            regExp = (ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.baseConfig && ReactiveFormConfig.json.baseConfig.dateFormat) ? this.getRegex(ReactiveFormConfig.json.baseConfig.dateFormat) : this.getRegex("mdy");
+        return regExp;
     }
 
-  getDate(value:string | Date,isBaseFormat:boolean = false): Date{
-    let year,month,day;
-      if (!this.isDate(value)){
-      let seperator:string;
-      let dateFormat:string;
-      if(ISO_DATE_REGEX.test(<string>value)){
-          return new Date(value);
-      }else{
-        seperator = ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.baseConfig && ReactiveFormConfig.json.baseConfig.seperator ? ReactiveFormConfig.json.baseConfig.seperator : "/";
-        dateFormat = ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.baseConfig && ReactiveFormConfig.json.baseConfig.dateFormat ? ReactiveFormConfig.json.baseConfig.dateFormat : "mdy";
-      }
-      
-      if(!isBaseFormat && ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.internationalization && ReactiveFormConfig.json.internationalization.dateFormat  && ReactiveFormConfig.json.internationalization.seperator)
-      {
-        seperator = ReactiveFormConfig.json.internationalization.seperator;
-        dateFormat = ReactiveFormConfig.json.internationalization.dateFormat;
-      }
-        switch(dateFormat){
-            case 'ymd':
-            [year, month, day] = (<String>value).split(seperator).map((val: string) => +val);
-            break;
-            case 'dmy':
-            [day,month,year] = (<String>value).split(seperator).map((val: string) => +val);
-            break;
-            case 'mdy':
-            [month,day,year] = (<String>value).split(seperator).map((val: string) => +val);
-            break;
-      }
-        return new Date(year,month-1,day);
-    }else
-      return <Date>value;
-  }
+    getDate(value: string | Date, isBaseFormat: boolean = false): Date {
+        let year, month, day;
+        if (!this.isDate(value)) {
+            let seperator: string;
+            let dateFormat: string;
+            if (ISO_DATE_REGEX.test(<string>value)) {
+                return new Date(value);
+            } else {
+                seperator = ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.baseConfig && ReactiveFormConfig.json.baseConfig.seperator ? ReactiveFormConfig.json.baseConfig.seperator : "/";
+                dateFormat = ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.baseConfig && ReactiveFormConfig.json.baseConfig.dateFormat ? ReactiveFormConfig.json.baseConfig.dateFormat : "mdy";
+            }
 
-    isValid(value: string | Date, config: any): Boolean{
-      if (typeof value == "string") {
-       // Fixed issue : https://github.com/rxweb/rxweb/issues/280 & feature request : https://github.com/rxweb/rxweb/issues/295
-      if(config && config.allowISODate && ISO_DATE_REGEX.test(<string>value))
-        return true;
-      let seperator = '/'
-      if(ReactiveFormConfig.json && ReactiveFormConfig.json.internationalization && ReactiveFormConfig.json.internationalization.seperator)
-        seperator = ReactiveFormConfig.json.internationalization.seperator;
-      value = value.replace(seperator,'-').replace(seperator,'-');
-      return this.regex().test(value);
-    }else
-      return this.isDate(value);
-  }
-
-  getConfigDateValue(config){
-    let date = config.value;
-    if(config.value && typeof config.value == "string"){
-      date = this.getDate(config.value,true);
+            if (!isBaseFormat && ReactiveFormConfig && ReactiveFormConfig.json && ReactiveFormConfig.json.internationalization && ReactiveFormConfig.json.internationalization.dateFormat && ReactiveFormConfig.json.internationalization.seperator) {
+                seperator = ReactiveFormConfig.json.internationalization.seperator;
+                dateFormat = ReactiveFormConfig.json.internationalization.dateFormat;
+            }
+            switch (dateFormat) {
+                case 'ymd':
+                    [year, month, day] = (<String>value).split(seperator).map((val: string) => +val);
+                    break;
+                case 'dmy':
+                    [day, month, year] = (<String>value).split(seperator).map((val: string) => +val);
+                    break;
+                case 'mdy':
+                    [month, day, year] = (<String>value).split(seperator).map((val: string) => +val);
+                    break;
+            }
+            return new Date(year, month - 1, day);
+        } else
+            return <Date>value;
     }
-    return date;
-  }
 
-   getCompareDate(config:any,control:any){
-          let date = this.getConfigDateValue(config);
-          if(config.fieldName){
-            let checkControl : any = ApplicationUtil.getFormControl(config.fieldName,control);
-              if(checkControl && checkControl.value){
-                  date = this.getDate(checkControl.value)
-                  }
-                }
+    isValid(value: string | Date, config: any): Boolean {
+        if (typeof value == "string") {
+            // Fixed issue : https://github.com/rxweb/rxweb/issues/280 & feature request : https://github.com/rxweb/rxweb/issues/295
+            if (config && config.allowISODate && ISO_DATE_REGEX.test(<string>value))
+                return true;
+            let seperator = '/'
+            if (ReactiveFormConfig.json && ReactiveFormConfig.json.internationalization && ReactiveFormConfig.json.internationalization.seperator)
+                seperator = ReactiveFormConfig.json.internationalization.seperator;
+            if (value.split(seperator).length !== 3)
+                return false;
+            value = value.replace(seperator, '-').replace(seperator, '-');
+            return this.regex().test(value);
+        } else
+            return this.isDate(value);
+    }
+
+    getConfigDateValue(config) {
+        let date = config.value;
+        if (config.value && typeof config.value == "string") {
+            date = this.getDate(config.value, true);
+        }
         return date;
-  }
+    }
+
+    getCompareDate(config: any, control: any) {
+        let date = this.getConfigDateValue(config);
+        if (config.fieldName) {
+            let checkControl: any = ApplicationUtil.getFormControl(config.fieldName, control);
+            if (checkControl && checkControl.value) {
+                date = this.getDate(checkControl.value)
+            }
+        }
+        return date;
+    }
 }
--- client-side/angular/test/reactive-form-validators/validator/date.validator.spec.ts
+++ client-side/angular/test/reactive-form-validators/validator/date.validator.spec.ts
@@ -1,137 +1,142 @@
-import { FormControl} from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 import { RxwebValidators, ReactiveFormConfig } from '../../../packages/reactive-form-validators';
 
-  describe('Validator', () => {
+describe('Validator', () => {
     beforeEach(() => {
-      ReactiveFormConfig.set({
-        "validationMessage": {
-          "date": "enter correct date.",
-        },
-        "baseConfig": {
-          "dateFormat": "dmy",
-          "seperator": "/"
-      }
-      });
-     
+        ReactiveFormConfig.set({
+            "validationMessage": {
+                "date": "enter correct date.",
+            },
+            "baseConfig": {
+                "dateFormat": "dmy",
+                "seperator": "/"
+            }
+        });
+
     });
 
     describe('dateValidator', () => {
 
-      it('should not error on an empty string.',
-        () => {
-          expect(RxwebValidators.date()(new FormControl(''))).toBeNull();
-        });
+        it('should not error on an empty string.',
+            () => {
+                expect(RxwebValidators.date()(new FormControl(''))).toBeNull();
+            });
 
-      it('should not error on null.',
-        () => {
-          expect(RxwebValidators.date()(new FormControl(null))).toBeNull();
-        });
+        it('should not error on null.',
+            () => {
+                expect(RxwebValidators.date()(new FormControl(null))).toBeNull();
+            });
 
-      it('should not error on undefined.',
-        () => {
-          expect(RxwebValidators.date()(new FormControl(undefined))).toBeNull();
-        });
+        it('should not error on undefined.',
+            () => {
+                expect(RxwebValidators.date()(new FormControl(undefined))).toBeNull();
+            });
 
         it("Should not error, valid date value",
-        () => { 
-          expect(RxwebValidators.date()(new FormControl('31/08/2018'))).toBeNull(); 
-        });
-      
+            () => {
+                expect(RxwebValidators.date()(new FormControl('31/08/2018'))).toBeNull();
+            });
+
         it("Should not error, valid date value",
-        () => { 
-          expect(RxwebValidators.date()(new FormControl(new Date()))).toBeNull(); 
-        });
-      
+            () => {
+                expect(RxwebValidators.date()(new FormControl(new Date()))).toBeNull();
+            });
+
 
         it("Should error, invalid date in the FormControl",
-        () => { 
-          expect(RxwebValidators.date()(new FormControl('2018/02/31'))).toEqual({'date':{ message: 'enter correct date.', refValues: [ '2018/02/31' ] } }); 
-        });
+            () => {
+                expect(RxwebValidators.date()(new FormControl('2018/02/31'))).toEqual({ 'date': { message: 'enter correct date.', refValues: ['2018/02/31'] } });
+            });
         it("Should not error, valid date format in ymd",
-        () => { 
-          ReactiveFormConfig.set({
-            "baseConfig": {
-              "dateFormat": "ymd",
-              "seperator": "/"
-          }
-          });
-         
-          expect(RxwebValidators.date()(new FormControl('2018/02/16'))).toBeNull(); 
-        });
+            () => {
+                ReactiveFormConfig.set({
+                    "baseConfig": {
+                        "dateFormat": "ymd",
+                        "seperator": "/"
+                    }
+                });
+
+                expect(RxwebValidators.date()(new FormControl('2018/02/16'))).toBeNull();
+            });
         it("Should not error, valid date format in mdy",
-        () => { 
-          ReactiveFormConfig.set({
-            "baseConfig": {
-              "dateFormat": "mdy",
-              "seperator": "/"
-          }
-          });
-         
-          expect(RxwebValidators.date()(new FormControl('02/16/2018'))).toBeNull(); 
-        });
-        
+            () => {
+                ReactiveFormConfig.set({
+                    "baseConfig": {
+                        "dateFormat": "mdy",
+                        "seperator": "/"
+                    }
+                });
+
+                expect(RxwebValidators.date()(new FormControl('02/16/2018'))).toBeNull();
+            });
+
         it("Should not error, valid date format in dmy",
-        () => { 
-          ReactiveFormConfig.set({
-            "baseConfig": {
-              "dateFormat": "dmy",
-              "seperator": "/"
-          }
-          });
-         
-          expect(RxwebValidators.date()(new FormControl('16/02/2018'))).toBeNull(); 
-        });
+            () => {
+                ReactiveFormConfig.set({
+                    "baseConfig": {
+                        "dateFormat": "dmy",
+                        "seperator": "/"
+                    }
+                });
+
+                expect(RxwebValidators.date()(new FormControl('16/02/2018'))).toBeNull();
+            });
 
         it("Should error, invalid date format in ymd",
-        () => { 
-          ReactiveFormConfig.set({
-            "validationMessage": {
-              "date": "enter correct date.",
-            },
-            "baseConfig": {
-              "dateFormat": "ymd",
-              "seperator": "/"
-          }
-          });
-          expect(RxwebValidators.date()(new FormControl('2018/16/16'))).toEqual({'date':{ message: 'enter correct date.', refValues: [ '2018/16/16' ] } }); 
-        });
+            () => {
+                ReactiveFormConfig.set({
+                    "validationMessage": {
+                        "date": "enter correct date.",
+                    },
+                    "baseConfig": {
+                        "dateFormat": "ymd",
+                        "seperator": "/"
+                    }
+                });
+                expect(RxwebValidators.date()(new FormControl('2018/16/16'))).toEqual({ 'date': { message: 'enter correct date.', refValues: ['2018/16/16'] } });
+            });
         it("Should error, invalid date format in mdy",
-        () => { 
-          ReactiveFormConfig.set({
-            "validationMessage": {
-              "date": "enter correct date.",
-            },
-            "baseConfig": {
-              "dateFormat": "mdy",
-              "seperator": "/"
-          }
-          });
-         
-          expect(RxwebValidators.date()(new FormControl('02/32/2018'))).toEqual({'date':{ message: 'enter correct date.', refValues: [ '02/32/2018' ] } }); 
-        });
-        
+            () => {
+                ReactiveFormConfig.set({
+                    "validationMessage": {
+                        "date": "enter correct date.",
+                    },
+                    "baseConfig": {
+                        "dateFormat": "mdy",
+                        "seperator": "/"
+                    }
+                });
+
+                expect(RxwebValidators.date()(new FormControl('02/32/2018'))).toEqual({ 'date': { message: 'enter correct date.', refValues: ['02/32/2018'] } });
+            });
+
         it("Should error, invalid date format in dmy",
-        () => { 
-          ReactiveFormConfig.set({
-            "validationMessage": {
-              "date": "enter correct date.",
-            },
-            "baseConfig": {
-              "dateFormat": "dmy",
-              "seperator": "/"
-          }
-          });
-         
-          expect(RxwebValidators.date()(new FormControl('16/20/2018'))).toEqual({'date':{ message: 'enter correct date.', refValues: [ '16/20/2018' ] } }); 
-        });
+            () => {
+                ReactiveFormConfig.set({
+                    "validationMessage": {
+                        "date": "enter correct date.",
+                    },
+                    "baseConfig": {
+                        "dateFormat": "dmy",
+                        "seperator": "/"
+                    }
+                });
+
+                expect(RxwebValidators.date()(new FormControl('16/20/2018'))).toEqual({ 'date': { message: 'enter correct date.', refValues: ['16/20/2018'] } });
+            });
 
         it("Should not error, valid ISO format date value",
-        () => { 
-            expect(RxwebValidators.date({allowISODate:true})(new FormControl("2018-09-15"))).toBeNull(); 
-        });
+            () => {
+                expect(RxwebValidators.date({ allowISODate: true })(new FormControl("2018-09-15"))).toBeNull();
+            });
+        // reference issue : https://github.com/rxweb/rxweb/issues/333
+        it("Should error, seperator is other than '/'",
+            () => {
+                expect(RxwebValidators.date({ allowISODate: false })(new FormControl("2018/09-15"))).toEqual({ 'date': { message: 'enter correct date.', refValues: ["2018/09-15"] } });
+            });
 
-      //end
+        //end
     });
 
-  });
+});
