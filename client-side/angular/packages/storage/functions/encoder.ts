90bd48a9c3c1e00e5c6adb30c6d0f3755fe5cfd5
ajayojha <ojhaajay@outlook.com>
2021-01-28 19:21:30 +0530

    fix: encoding the text #459

--- client-side/angular/packages/storage/functions/encoder.ts
+++ client-side/angular/packages/storage/functions/encoder.ts
@@ -1,7 +1,7 @@
 const EQUAL_TO = '=';
-const DOUBLE_ZERO = '00';
+const UNDERSCORE = "_";
 export function encoder(value, isEncode: boolean) {
     if (value)
-        return isEncode ? btoa(value).replace(EQUAL_TO, DOUBLE_ZERO).replace(EQUAL_TO, DOUBLE_ZERO) : atob(value.replace(DOUBLE_ZERO, EQUAL_TO).replace(DOUBLE_ZERO, EQUAL_TO));
+        return isEncode ? btoa(value).replace(EQUAL_TO, UNDERSCORE).replace(EQUAL_TO, UNDERSCORE) : atob(value.replace(UNDERSCORE, EQUAL_TO).replace(UNDERSCORE, EQUAL_TO));
     return '';
 }
