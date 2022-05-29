78275a115a949944581c02591c9b1fdc50a01d13
Pranav I <pranav.irabatti7@gmail.com>
2022-05-29 19:08:36 +0530

    fix(validator): locale property in validator not validating for all alphabet locales

--- client-side/angular/packages/reactive-form-validators/util/alphabet-regex.locale.ts
+++ client-side/angular/packages/reactive-form-validators/util/alphabet-regex.locale.ts
@@ -1,34 +1,34 @@
 ﻿export const alphabet = {
-    'danish': /^[A-ZÆØÅ]+$/,
-    'french': /^[A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ]+$/,
-    'german': /^[A-ZÄÖÜß]+$/,
+    'danish': /^[A-ZÆØÅ]+$/i,
+    'french': /^[A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ]+$/i,
+    'german': /^[A-ZÄÖÜß]+$/i,
     'spanish': /^[a-zñáéíóúü]+$/i,
-    'russian': /^[А-ЯЁ]+$/,
+    'russian': /^[А-ЯЁ]+$/i,
 
 };
 
 export const alphaWithWhitespace = {
-    'danish': /^[A-ZÆØÅ\s]+$/,
-    'french': /^[A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ\s]+$/,
-    'german': /^[A-ZÄÖÜß\s]+$/,
+    'danish': /^[A-ZÆØÅ\s]+$/i,
+    'french': /^[A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ\s]+$/i,
+    'german': /^[A-ZÄÖÜß\s]+$/i,
     'spanish': /^[a-zñáéíóúü\s]+$/i,
-    'russian': /^[А-ЯЁ\s]+$/,
+    'russian': /^[А-ЯЁ\s]+$/i,
 
 };
 
 export const alphanumeric = {
-    'danish': /^[0-9A-ZÆØÅ]+$/,
-    'french': /^[0-9A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ]+$/,
-    'german': /^[0-9A-ZÄÖÜß]+$/,
+    'danish': /^[0-9A-ZÆØÅ]+$/i,
+    'french': /^[0-9A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ]+$/i,
+    'german': /^[0-9A-ZÄÖÜß]+$/i,
     'spanish': /^[0-9a-zñáéíóúü]+$/i,
-    'russian': /^[0-9А-ЯЁ]+$/,
+    'russian': /^[0-9А-ЯЁ]+$/i,
 };
 
 export const alphanumericWithWitespace = {
-    'danish': /^[0-9A-ZÆØÅ]+$/,
-    'french': /^[0-9A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ]+$/,
-    'german': /^[0-9A-ZÄÖÜß]+$/,
+    'danish': /^[0-9A-ZÆØÅ\s]+$/i,
+    'french': /^[0-9A-ZÀÂÆÇÉÈÊËÏÎÔŒÙÛÜŸ\s]+$/i,
+    'german': /^[0-9A-ZÄÖÜß\s]+$/i,
     'spanish': /^[0-9a-zñáéíóúü\s]+$/i,
-    'russian': /^[0-9А-ЯЁ]+$/,
+    'russian': /^[0-9А-ЯЁ\s]+$/i,
 };
 
